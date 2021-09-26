96275834ccd7f3aded5e8a77d0f34482083e8d5a
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2021-09-26 14:12:40 -0500

    Update error_creators.cpp

--- src/error_creators.cpp
+++ src/error_creators.cpp
@@ -19,7 +19,7 @@ plasma::vm::value *
 plasma::vm::virtual_machine::new_key_not_found_error(plasma::vm::context *c, plasma::vm::value *key) {
     value *errorType = this->force_any_from_master(c, KeyNotFoundError);
     value *result = this->force_construction(c, errorType);
-    this->force_initialization(c, result, std::vector < value * > {key});
+    this->force_initialization(c, result, std::vector<value *>{key});
     return result;
 }
 
@@ -28,7 +28,7 @@ plasma::vm::virtual_machine::new_index_out_of_range_error(plasma::vm::context *c
                                                           size_t requestedIndex) {
     value *errorType = this->force_any_from_master(c, IndexOutOfRangeError);
     value *result = this->force_construction(c, errorType);
-    this->force_initialization(c, result, std::vector < value * > {
+    this->force_initialization(c, result, std::vector<value *>{
             this->new_integer(c, false, length),
             this->new_integer(c, false, requestedIndex)
     });
@@ -39,7 +39,7 @@ plasma::vm::value *
 plasma::vm::virtual_machine::new_unhashable_type_error(plasma::vm::context *c, plasma::vm::value *objectType) {
     value *errorType = this->force_any_from_master(c, UnhashableTypeError);
     value *result = this->force_construction(c, errorType);
-    this->force_initialization(c, result, std::vector < value * > {
+    this->force_initialization(c, result, std::vector<value *>{
             objectType
     });
     return result;
@@ -49,7 +49,7 @@ plasma::vm::value *
 plasma::vm::virtual_machine::new_not_implemented_callable_error(plasma::vm::context *c, std::string symbol) {
     value *errorType = this->force_any_from_master(c, NotImplementedCallableError);
     value *result = this->force_construction(c, errorType);
-    this->force_initialization(c, result, std::vector < value * > {
+    this->force_initialization(c, result, std::vector<value *>{
             this->new_string(c, false, symbol)
     });
     return result;
@@ -60,7 +60,7 @@ plasma::vm::virtual_machine::new_invalid_number_of_arguments_error(plasma::vm::c
                                                                    size_t received) {
     value *errorType = this->force_any_from_master(c, InvalidNumberOfArgumentsError);
     value *result = this->force_construction(c, errorType);
-    this->force_initialization(c, result, std::vector < value * > {
+    this->force_initialization(c, result, std::vector<value *>{
             this->new_integer(c, false, received),
             this->new_integer(c, false, expected)
     });
@@ -72,7 +72,7 @@ plasma::vm::virtual_machine::new_object_with_name_not_found_error(context *c, va
                                                                   const std::string &symbol) {
     value *errorType = this->force_any_from_master(c, ObjectWithNameNotFoundError);
     value *result = this->force_construction(c, errorType);
-    this->force_initialization(c, result, std::vector < value * > {
+    this->force_initialization(c, result, std::vector<value *>{
             source->get_type(c, this), this->new_string(c, false, symbol)
     });
     return result;
@@ -82,7 +82,7 @@ plasma::vm::value *
 plasma::vm::virtual_machine::new_object_with_name_not_found_error(context *c, const std::string &symbol) {
     value *errorType = this->force_any_from_master(c, ObjectWithNameNotFoundError);
     value *result = this->force_construction(c, errorType);
-    this->force_initialization(c, result, std::vector < value * > {
+    this->force_initialization(c, result, std::vector<value *>{
             get_none(c)->get_type(c, this), this->new_string(c, false, symbol)
     });
     return result;
@@ -103,7 +103,7 @@ plasma::vm::virtual_machine::new_invalid_type_error(plasma::vm::context *c, plas
         }
         expecting += expectedType;
     }
-    this->force_initialization(c, result, std::vector < value * > {
+    this->force_initialization(c, result, std::vector<value *>{
             receivedType,
             this->new_string(c, false, expecting)
     });
@@ -115,7 +115,7 @@ plasma::vm::virtual_machine::new_object_construction_error(plasma::vm::context *
                                                            const std::string &errorMessage) {
     value *errorType = this->force_any_from_master(c, ObjectConstructionError);
     value *result = this->force_construction(c, errorType);
-    this->force_initialization(c, result, std::vector < value * > {
+    this->force_initialization(c, result, std::vector<value *>{
             type, this->new_string(c, false, errorMessage)
     });
     return result;
@@ -126,7 +126,7 @@ plasma::vm::virtual_machine::new_builtin_symbol_protection_error(plasma::vm::con
                                                                  const std::string &symbol) {
     value *errorType = this->force_any_from_master(c, BuiltInSymbolProtectionError);
     value *result = this->force_construction(c, errorType);
-    this->force_initialization(c, result, std::vector < value * > {
+    this->force_initialization(c, result, std::vector<value *>{
             source->type, this->new_string(c, false, symbol)
     });
     return result;
@@ -136,7 +136,7 @@ plasma::vm::value *
 plasma::vm::virtual_machine::new_object_not_callable_error(plasma::vm::context *c, plasma::vm::value *objectType) {
     value *errorType = this->force_any_from_master(c, ObjectNotCallableError);
     value *result = this->force_construction(c, errorType);
-    this->force_initialization(c, result, std::vector < value * > {
+    this->force_initialization(c, result, std::vector<value *>{
             objectType
     });
     return result;
